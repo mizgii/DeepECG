1de68a9c2a774bc66d409525ea70cfd6eddcbb0a
mizgii <62216003+mizgii@users.noreply.github.com>
2024-02-19 17:57:40 +0100

    Update run_exp_1.py

--- run_exp_1.py
+++ run_exp_1.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
                                                               NUM_EPOCHS=10, 
                                                               DATA_PATH = path, 
                                                               FS=128,
-                                                              NUM_PAT=patient)
+                                                              NUM_PATIENTS=patient)
         with open('exp1_results/result-III-V3-V5.txt', 'a') as f:
             f.write(f"With number patients: {patient}:\n")
             f.write(f"Accuracy: {accuracy}, Time train: {total_train_time}, Time eval: {total_eval_time}\n\n")
@@ -32,7 +32,7 @@ if __name__ == "__main__":
                                                               NUM_EPOCHS=10, 
                                                               DATA_PATH = path, 
                                                               FS=128,
-                                                              NUM_PAT=patient)
+                                                              NUM_PATIENTS=patient)
         with open('exp1_results/result-III-V3.txt', 'a') as f:
             f.write(f"With number patients: {patient}:\n")
             f.write(f"Accuracy: {accuracy}, Time train: {total_train_time}, Time eval: {total_eval_time}\n\n")
@@ -45,7 +45,7 @@ if __name__ == "__main__":
                                                               NUM_EPOCHS=10, 
                                                               DATA_PATH = path, 
                                                               FS=128,
-                                                              NUM_PAT=patient)
+                                                              NUM_PATIENTS=patient)
         with open('exp1_results/result-V5.txt', 'a') as f:
             f.write(f"With number patients: {patient}:\n")
             f.write(f"Accuracy: {accuracy}, Time train: {total_train_time}, Time eval: {total_eval_time}\n\n")
@@ -58,7 +58,7 @@ if __name__ == "__main__":
                                                               NUM_EPOCHS=10, 
                                                               DATA_PATH = path, 
                                                               FS=128,
-                                                              NUM_PAT=patient)
+                                                              NUM_PATIENTS=patient)
         with open('exp1_results/result-V3.txt', 'a') as f:
             f.write(f"With number patients: {patient}:\n")
             f.write(f"Accuracy: {accuracy}, Time train: {total_train_time}, Time eval: {total_eval_time}\n\n")
@@ -71,7 +71,7 @@ if __name__ == "__main__":
                                                               NUM_EPOCHS=10, 
                                                               DATA_PATH = path, 
                                                               FS=128,
-                                                              NUM_PAT=patient)
+                                                              NUM_PATIENTS=patient)
         with open('exp1_results/result-III.txt', 'a') as f:
             f.write(f"With number patients: {patient}:\n")
             f.write(f"Accuracy: {accuracy}, Time train: {total_train_time}, Time eval: {total_eval_time}\n\n")
